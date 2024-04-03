9a4065cb5fcf62930a91965697e66ba967e763b7
Your Name <fahimrashid350@gmail.com>
2024-04-03 16:33:40 +0600

    coupon section is working

--- js/onclick.js
+++ js/onclick.js
@@ -2,7 +2,7 @@ function myFunction(name,price){
     
     const itemName= document.getElementById(name).innerText;
    
-    console.log(itemName);
+    // console.log(itemName);
     
 
     // get the item price
@@ -35,33 +35,82 @@ function myFunction(name,price){
 
     // console.log(totalInt);
 
-
     // set new total price
     const newItemPrice=itemPriceInt+totalPriceInt;
     totalPriceField.innerText=newItemPrice;
     // console.log(newItemPrice);
 
+    // reset total and discount field 
+    totalField.innerText=newItemPrice;
+    DiscountField.innerText='00';
+
     if(newItemPrice>=200){
 
-    const couponInputField=document.getElementById('coupon-input');
-    couponInputField.disabled=false;
-
-    
-
-
-    // set new discount
-    const newDiscount1Percent=newItemPrice/100;
-    const newDiscount=newDiscount1Percent*20;
-    DiscountField.innerText=newDiscount;
-
-    // set new total
-    const newTotalTemp=newItemPrice-newDiscount;
-    const total=newTotalTemp+DiscountInt;
-    totalField.innerText=total;
-
-
-
+        const couponInputField=document.getElementById('coupon-input');
+        couponInputField.disabled=false;
 
     }
 
+   
+    
+    document.getElementById('discount-apply').addEventListener('click',function(){
+
+        const couponInputField=document.getElementById('coupon-input');
+        const coupon=couponInputField.value;
+        const validCoupon='SELL200';
+
+        if(coupon==validCoupon){
+            // set new discount
+        const newDiscount1Percent=newItemPrice/100;
+        const newDiscount=newDiscount1Percent*20;
+        DiscountField.innerText=newDiscount;
+    
+        // set new total
+        const newTotalTemp=newItemPrice-newDiscount;
+        const total=newTotalTemp+DiscountInt;
+        totalField.innerText=total;
+        return;
+        }
+        else if(coupon==''){
+            alert('Input coupon');
+            return;
+        }
+        else{
+            alert('Invalid coupon');
+            return;
+        }
+        
+
+    })
+
+  
+
+    // add to item cart section
+    addToCart(itemName,itemPriceInt);
+
 }
+function addToCart(name,price){
+    const itemListSection=document.getElementById('item_list');
+    const p=document.createElement('p');
+    p.innerHTML=`${name} ${price} <button id="quantity" class="btn btn-ghost">Remove</button>`;
+    itemListSection.appendChild(p);
+
+
+}
+
+
+
+
+document.getElementById('sell200-btn').addEventListener('click',function(){
+    const totalPriceField=document.getElementById('total-price');
+    const totalPriceString=totalPriceField.innerText;
+    const totalPriceInt=parseFloat(totalPriceString);
+    
+    if(totalPriceInt>=200){
+        const couponInputField=document.getElementById('coupon-input');
+        couponInputField.value='SELL200';
+    }
+    if(totalPriceInt==0){
+        alert("Select item to use coupon");
+    }
+})
